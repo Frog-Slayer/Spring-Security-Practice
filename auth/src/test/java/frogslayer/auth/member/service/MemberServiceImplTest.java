324a1465e8e9122aef8923b04545ae795645b56e
박영서 <pj0642@gmail.com>
2023-10-14 22:57:11 +0900

    test : MemberService의 이메일 검증 메서드 테스트

--- auth/src/test/java/frogslayer/auth/member/service/MemberServiceImplTest.java
+++ auth/src/test/java/frogslayer/auth/member/service/MemberServiceImplTest.java
@@ -1,6 +1,8 @@
 package frogslayer.auth.member.service;
 
 import frogslayer.auth.AuthApplication;
+import frogslayer.auth.member.exception.exceptions.InvalidEmailFormatException;
+import frogslayer.auth.member.exception.exceptions.InvalidPasswordFormatException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,14 +21,55 @@ class MemberServiceImplTest {
     @Test
     void testEmailValidationWithBlank() {
         String email = "";
-        assertThrows(Exception.class, () -> memberService.verifyEmailFormat(email));
+        assertThrows(InvalidEmailFormatException.class, () -> memberService.verifyEmailFormat(email));
     }
 
     @DisplayName("도메인 없는 이메일 가입 시도 테스트")
     @Test
     void testEmailValidationWithoutEmailDomain() {
         String email = "email";
-        assertThrows(Exception.class, () -> memberService.verifyEmailFormat(email));
+        assertThrows(InvalidPasswordFormatException.class, () -> memberService.verifyEmailFormat(email));
     }
 
+    @DisplayName("최상위 도메인이 없는 이메일 가입 시도 테스트")
+    @Test
+    void testEmailValidationWithoutTLD() {
+        String email = "email@dom";
+        assertThrows(InvalidEmailFormatException.class, () -> memberService.verifyEmailFormat(email));
+    }
+
+    @DisplayName("최상위 도메인만 있는 이메일 가입 시도 테스트")
+    @Test
+    void testEmailValidationOnlyWithTLD() {
+        String email = "email@.com";
+        assertThrows(InvalidEmailFormatException.class, () -> memberService.verifyEmailFormat(email));
+    }
+
+    @DisplayName(". 뒤에 아무 것도 없는 경우 테스트")
+    @Test
+    void testEmailValidationNothingAfterDot() {
+        String email = "email@dom.";
+        assertThrows(InvalidEmailFormatException.class, () -> memberService.verifyEmailFormat(email));
+    }
+
+    @DisplayName("도메인만 있는 경우 테스트")
+    @Test
+    void testEmailValidationOnlyWithDomain() {
+        String email = "@dom.com";
+        assertThrows(InvalidEmailFormatException.class, () -> memberService.verifyEmailFormat(email));
+    }
+
+    @DisplayName("@가 두 개 이상 있는 경우 테스트")
+    @Test
+    void testEmailValidationWithMultipleAtS() {
+        String email = "user@name@dom.com";
+        assertThrows(InvalidEmailFormatException.class, () -> memberService.verifyEmailFormat(email));
+    }
+
+    @DisplayName(".이 두 개 이상 있는 경우 테스트")
+    @Test
+    void testEmailValidationWithMultipleDots() {
+        String email = "username@dom.co.m";
+        assertThrows(InvalidEmailFormatException.class, () -> memberService.verifyEmailFormat(email));
+    }
 }

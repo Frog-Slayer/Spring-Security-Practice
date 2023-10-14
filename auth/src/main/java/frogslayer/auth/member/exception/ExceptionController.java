092da5a87dec9b572af77a7f7adf0f4004c34f21
박영서 <pj0642@gmail.com>
2023-10-14 16:33:35 +0900

    feat : ExceptionController를 추가 - Custom Exception들에 대한 Handler들을 작성

---
+++ auth/src/main/java/frogslayer/auth/member/exception/ExceptionController.java
@@ -0,0 +1,38 @@
+package frogslayer.auth.member.exception;
+
+import frogslayer.auth.member.exception.exceptions.DuplicateUserException;
+import frogslayer.auth.member.exception.exceptions.InvalidEmailFormatException;
+import frogslayer.auth.member.exception.exceptions.InvalidPasswordFormatException;
+import frogslayer.auth.member.exception.exceptions.NoSuchUserException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice("frogslayer.auth")
+public class ExceptionController {
+    @ExceptionHandler(DuplicateUserException.class)
+    public ResponseEntity<String> handleDuplicateUserException(DuplicateUserException err){
+        return getResponseEntity(ExceptionCode.DUPLICATE_USER_EXCEPTION);
+    }
+
+    @ExceptionHandler(InvalidEmailFormatException.class)
+    public ResponseEntity<String>  handleInvalidEmailFormatException(InvalidEmailFormatException err){
+        return getResponseEntity(ExceptionCode.INVALID_EMAIL_FORMAT_EXCEPTION);
+    }
+
+    @ExceptionHandler(InvalidPasswordFormatException.class)
+    public ResponseEntity<String> handleInvalidPasswordFormatException(InvalidPasswordFormatException err){
+        return getResponseEntity(ExceptionCode.INVALID_PASSWORD_FORMAT_EXCEPTION);
+    }
+
+    @ExceptionHandler(InvalidEmailFormatException.class)
+    public ResponseEntity<String> handleNoSuchUserException(NoSuchUserException err){
+        return getResponseEntity(ExceptionCode.NO_SUCH_USER_EXCEPTION);
+    }
+
+    public static ResponseEntity<String> getResponseEntity(ExceptionCode code){
+        return ResponseEntity
+                .status(code.getHttpStatusCode())
+                .body(code.getMessage());
+    }
+}

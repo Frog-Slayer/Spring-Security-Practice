1adba917334d827f570cbdfb898a4077e2e62d19
frog-slayer <pj0642@gmail.com>
2024-06-17 17:58:52 +0900

    test: JwtService#extractName() test - fix: application.yml의 jew.secret-key가 HMAC-SHA-512에서 요구하는 최소 키 길이를 충족하도록 수정

--- backend/src/test/java/security/practice/global/auth/service/jwt/JwtServiceImplTest.java
+++ backend/src/test/java/security/practice/global/auth/service/jwt/JwtServiceImplTest.java
@@ -1,17 +1,25 @@
 package security.practice.global.auth.service.jwt;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
 class JwtServiceImplTest {
 
-
-    @Test
-    void generateAccessToken() {
-    }
+    @Autowired
+    private JwtService jwtService;
 
     @Test
     void extractName() {
+        String username = "pj0642@gmail.com";
+        String accessToken = jwtService.generateAccessToken(username);
+        assertEquals(username, jwtService.extractName(accessToken).get());
     }
 
     @Test

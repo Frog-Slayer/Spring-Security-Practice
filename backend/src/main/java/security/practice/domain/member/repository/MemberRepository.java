80a7f8b3cb4d6623eb93207d1cbf0e48e9089eb7
frog-slayer <pj0642@gmail.com>
2024-06-17 15:07:42 +0900

    feat: implement CustomUserDetailsService - MemberRepository에 findByUsername 메서드를 추가

--- backend/src/main/java/security/practice/domain/member/repository/MemberRepository.java
+++ backend/src/main/java/security/practice/domain/member/repository/MemberRepository.java
@@ -4,9 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import security.practice.domain.member.entity.Member;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    Optional<Member> findByUsername(String username);
     boolean existsByUsername(String username);
 
 }
---
+++ backend/src/main/java/security/practice/global/auth/service/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package security.practice.global.auth.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import security.practice.domain.member.entity.Member;
+import security.practice.domain.member.repository.MemberRepository;
+
+import java.util.function.Supplier;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Supplier<UsernameNotFoundException> s = () -> new UsernameNotFoundException("username not found");
+        Member member = memberRepository.findByUsername(username).orElseThrow(s);
+        return User.builder()
+                .username(member.getUsername())
+                .password(member.getPassword())
+                .roles(member.getRole().name())
+                .build();
+    }
+}

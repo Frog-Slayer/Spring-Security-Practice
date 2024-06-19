3c062ab7378ab18620536c175e57affba462ba8a
frog-slayer <pj0642@gmail.com>
2024-06-19 20:01:21 +0900

    feat: implement JwtAuthenticationProvider - 이 클래스가 인증 역할을 담당

---
+++ backend/src/main/java/security/practice/global/auth/provider/JwtAuthenticationProvider.java
@@ -0,0 +1,43 @@
+package security.practice.global.auth.provider;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.util.ClassUtils;
+import security.practice.global.auth.service.jwt.JwtService;
+import security.practice.global.auth.token.JwtAuthenticationToken;
+import security.practice.global.auth.token.PreJwtAuthenticationToken;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationProvider implements AuthenticationProvider {
+
+    private final JwtService jwtService;
+    private UserDetailsService userDetailsService;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String accessToken = authentication.getPrincipal().toString();
+        UserDetails userDetails = retrieveUser(accessToken);
+        return new JwtAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+    }
+
+    protected UserDetails retrieveUser(String accessToken) throws AuthenticationException {
+        return jwtService.extractName(accessToken)
+                .map(userDetailsService::loadUserByUsername)
+                .orElseThrow(()-> new AuthenticationServiceException("Could not extract username from JWT token"));
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return ClassUtils.isAssignable(PreJwtAuthenticationToken.class, authentication);
+    }
+
+    public void setUserDetailsService(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+}

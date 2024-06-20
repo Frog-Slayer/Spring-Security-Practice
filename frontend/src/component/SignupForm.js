e8bab13e11b7e819b30dc30ef5f56c9d7809ff32
frog-slayer <pj0642@gmail.com>
2024-06-20 23:22:20 +0900

    feat: add refresh button

--- frontend/src/component/SignupForm.js
+++ frontend/src/component/SignupForm.js
@@ -5,6 +5,7 @@ import axios from 'axios'
 function SignupForm() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [accessToken, setAccesstoken] = useState('');
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -31,17 +32,56 @@ function SignupForm() {
             password: password
         };
 
-        axios.post('http://localhost:8080/login', body)
+        axios.post('http://localhost:8080/login', body,
+            { withCredentials: true }, 
+        )
             .then((res) => {
                 if (res.status === 200) {
                     let accessToken = res.headers['authorization'];
-                    console.log(accessToken);
+                    setAccesstoken(accessToken);
+                    // console.log(accessToken);
+                    console.log(res);
                 }
             })
             .catch((res) => {
                 console.log(res);
             })
     }
+    
+    const handleAuthenticatedApiButton = async (e) => {
+        e.preventDefault();
+
+        axios.get('http://localhost:8080/member/test', {
+            headers: {
+                Authorization: accessToken
+            },
+        })
+            .then((res) => {
+                if (res.status === 200) {
+                    console.log(res.data)
+                }
+            })
+            .catch((res) => {
+                console.log(res);
+            })
+    }
+
+    const handleRefresh = async (e) => {
+        e.preventDefault();
+
+        axios.get('http://localhost:8080/refresh', { withCredentials: true })
+            .then((res) => {
+                if (res.status === 200) {
+                    let accessToken = res.headers['authorization'];
+                    setAccesstoken(accessToken);
+                    console.log(accessToken)
+                }
+            })
+            .catch((res) => {
+                console.log(res);
+            })
+    }
+
 
     return (
         <div className="formContainer">
@@ -54,6 +94,8 @@ function SignupForm() {
                 </div>
                 <button onClick={handleSubmit} className='submitButton' type='submit'> sign up </button>
                 <button onClick={handleLogin} className='submitButton' type='submit'> login </button>
+                <button onClick={handleAuthenticatedApiButton} className='submitButton' type='button'> api </button>
+                <button onClick={handleRefresh} className='submitButton' type='button'> refresh </button>
             </form>
         </div>
     )

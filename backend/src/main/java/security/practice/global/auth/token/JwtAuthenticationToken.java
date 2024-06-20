addb3d6c120295e153ee1b11f915e2c0ec587214
frog-slayer <pj0642@gmail.com>
2024-06-20 21:41:25 +0900

    fix: modify access modifier of constructors to protected

--- backend/src/main/java/security/practice/global/auth/token/JwtAuthenticationToken.java
+++ backend/src/main/java/security/practice/global/auth/token/JwtAuthenticationToken.java
@@ -12,14 +12,14 @@ public class JwtAuthenticationToken extends AbstractAuthenticationToken {
     private final Object principal;
     private final Object credentials;
 
-    public JwtAuthenticationToken(Object principal, Object credentials) {
+    protected JwtAuthenticationToken(Object principal, Object credentials) {
         super(null);
         setAuthenticated(false);
         this.principal = principal;
         this.credentials = credentials;
     }
 
-    public JwtAuthenticationToken(Object principal, Object credentials, Collection<? extends GrantedAuthority> authorities) {
+    protected JwtAuthenticationToken(Object principal, Object credentials, Collection<? extends GrantedAuthority> authorities) {
         super(authorities);
         setAuthenticated(true);
         this.principal = principal;
